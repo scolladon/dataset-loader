216a02bd7df67151b2ef9294c44978e61b472011
Sébastien Colladon <colladonsebastien@gmail.com>
2026-04-02 16:11:25 +0200

    fix: correct knip config entries and ignored dependencies

--- knip.config.ts
+++ knip.config.ts
@@ -1,12 +1,12 @@
 export default {
   entry: [
     'src/commands/**/*.ts',
-    'bin/dev.js',
     '**/*.{nut,test}.ts',
     'test/perf/**/*.{ts,mjs}',
+    'vitest.config.perf.ts',
     '.github/**/*.yml',
   ],
   project: ['**/*.{ts,js,json,yml}'],
-  ignoreDependencies: ['@commitlint/config-conventional'],
-  ignoreBinaries: ['commitlint', 'npm-check-updates'],
+  ignoreDependencies: ['sinon'],
+  ignoreBinaries: ['npm-check-updates'],
 }
